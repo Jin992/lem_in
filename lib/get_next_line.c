92beef9a102edea79dd74e5165234cc6ba5176cc
Evgen ARTESHCHUK <earteshc@e3r6p1.unit.ua>
2018-03-14 19:32:24 +0200

    add input

---
+++ lib/get_next_line.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earteshc <earteshc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/14 15:21:44 by earteshc          #+#    #+#             */
+/*   Updated: 2017/12/18 10:56:25 by earteshc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+int			get_next_line(int fd, char **line)
+{
+	static char	*arr[100];
+	char		*tmp;
+	char		buff[BUFF_SIZE + 1];
+	int			res;
+
+	if (fd < 0 || !line || BUFF_SIZE < 1 ||
+			(res = read(fd, buff, BUFF_SIZE)) == -1)
+		return (-1);
+	arr[fd] = arr[fd] ? arr[fd] : ft_strdup("");
+	tmp = arr[fd];
+	buff[res] = '\0';
+	arr[fd] = ft_strjoin(arr[fd], buff);
+	free(tmp);
+	tmp = arr[fd];
+	if (!ft_strlen(arr[fd]))
+		return (0);
+	else if (ft_strchr(arr[fd], '\n') &&
+	(*line = ft_strsub(arr[fd], 0, ft_strchr(arr[fd], '\n') - arr[fd])))
+		arr[fd] = ft_strdup(ft_strchr(tmp, '\n') + 1);
+	else if (!res && ft_strlen(arr[fd]) && (*line = ft_strdup(arr[fd])))
+		arr[fd] = NULL;
+	else
+		return (get_next_line(fd, line));
+	free(tmp);
+	return (1);
+}
--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 
 #include <stdio.h>
+#include "lib/includes/libft.h"
+#include "lib/includes/ft_printf.h"
 #include <stdlib.h>
 
 #define INF 2147483647
 
 typedef  struct s_room
 {
-    int             room_id;
     char            *label;
     struct s_room   **link;
     int             distance;
+    int             x;
+    int             y;
     int             mark;
+    int             link_qnt;
 }               t_room;
 
 typedef struct  s_lem
@@ -20,6 +24,7 @@ typedef struct  s_lem
     t_room  **room;
     t_room  *start;
     t_room  *end;
+    int     room_input;
     int     distance;
 
 }               t_lem;
@@ -57,20 +62,232 @@ int find_route(t_room *data, t_lem *lem)
     return (0);
 }
 
-int lead_ants(t_lem *data)
+int input_error(char **res)
 {
+    ft_free_str_arr(res);
+    write(1, "ERROR\n", 6);
+    return (1);
+}
 
+int check_digit(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+        if (!ft_isdigit(str[i++]))
+            return (1);
+    return (0);
+}
+
+int check_ants(char *line, t_lem *lem)
+{
+    char **res;
+
+    res = ft_strsplit(line, ' ');
+    if (res[1] == NULL && ft_isdigit(res[0][0]))
+    {
+        if (check_digit(res[0]))
+            return (input_error(res));
+        lem->ant_qnt = (int) ft_atoi(res[0]);
+        ft_free_str_arr(res);
+        ft_putstr(line);
+        ft_putstr("\n");
+    }
+    else
+        return (input_error(res));
+    return (0);
+}
+
+
+int room_realloc(t_lem *lem, t_room *add)
+{
+    t_room **tmp;
+    int i;
+
+    i = -1;
+    if (lem->room != NULL)
+    {
+        tmp = (t_room **)malloc((sizeof(t_room *)) * (lem->room_qnt + 1));
+        tmp[lem->room_qnt] = 0;
+        while (lem->room[++i] != 0)
+            tmp[i] = lem->room[i];
+        lem->room = (t_room **)malloc((sizeof(t_room *)) * (lem->room_qnt + 1 + 1));
+        lem->room[lem->room_qnt + 1] = 0;
+        i = -1;
+        while (tmp[++i] != 0)
+            lem->room[i] = tmp[i];
+        lem->room[i] = add;
+        free(tmp);
+    }
+    else
+    {
+        lem->room = (t_room **)malloc(sizeof(t_room *) * 2);
+        lem->room[1] = 0;
+        lem->room[0] = add;
+    }
+    return (0);
+}
+
+int check_rooms(char **line, t_lem *lem)
+{
+    char **res;
+    int i;
+    int start_flag;
+    int end_flag;
+
+    start_flag = 0;
+    end_flag = 0;
+    i = 0;
+    res = ft_strsplit(*line, ' ');
+    if (res[1] == 0 && (ft_strcmp(res[0], "##start") == 0 || ft_strcmp(res[0], "##end") == 0))
+    {
+        if (ft_strcmp(res[0], "##start") == 0)
+        {
+            ft_free_str_arr(res);
+            free(*line);
+            while (get_next_line(0, line) == 1)
+            {
+                if ((*line)[0] != '#')
+                    break;
+                free (*line);
+            }
+            ft_putstr("start\n");
+            res = ft_strsplit(*line, ' ');
+            start_flag = 1;
+
+        }
+        if (ft_strcmp(res[0], "##end") == 0)
+        {
+            ft_free_str_arr(res);
+            free(*line);
+            while (get_next_line(0, line) == 1)
+            {
+                if ((*line)[0] != '#')
+                    break;
+                free (*line);
+            }
+            ft_putstr("end\n");
+            res = ft_strsplit(*line, ' ');
+            end_flag = 1;
+        }
+    }
+    while (res[i] != 0)
+        i++;
+    if (i != 3 || check_digit(res[1]) || check_digit(res[2]))
+        return (input_error(res));
+    t_room *new_room;
+    new_room = (t_room *)malloc(sizeof(t_room) * 1);
+    new_room->label = ft_strdup(res[0]);
+    new_room->x = (int)ft_atoi(res[1]);
+    new_room->y = (int)ft_atoi(res[2]);
+    new_room->link = 0;
+    ft_printf("Label: %s x: %d y: %d\n", new_room->label, new_room->x, new_room->y);
+    room_realloc(lem, new_room);
+    if (lem->start == NULL && start_flag == 1)
+    {
+        lem->start = new_room;
+        ft_printf("start room label %s\n", lem->start->label );
+    }
+    if (lem->end == NULL && end_flag == 1)
+    {
+        lem->end = new_room;
+        ft_printf("end room label %s\n", lem->end->label );
+    }
+    i = 0;
+    while (lem->room[i] != 0)
+        printf("label %s \n", lem->room[i++]->label);
+    lem->room_qnt++;
+    return (0);
+}
+
+
+
+int link_realloc(t_lem *lem, t_room *add, int vertex)
+{
+    struct s_room **tmp;
+    int i;
+
+    i = -1;
+    if (lem->room[vertex]->link != NULL)
+    {
+        tmp = (struct s_room**)malloc((sizeof(struct s_room*)) * (lem->room[vertex]->link_qnt + 1));
+        tmp[lem->room[vertex]->link_qnt] = 0;
+        while (lem->room[vertex]->link[++i] != 0)
+            tmp[i] = lem->room[vertex]->link[i];
+        lem->room[vertex]->link = (struct s_room **)malloc((sizeof(struct s_room*)) * (lem->room[vertex]->link_qnt + 1 + 1));
+        lem->room[vertex]->link[lem->room[vertex]->link_qnt + 1] = 0;
+        i = -1;
+        while (tmp[++i] != 0)
+            lem->room[vertex]->link[i] = tmp[i];
+        lem->room[vertex]->link[i] = add;
+        free(tmp);
+    }
+    else
+    {
+        lem->room[vertex]->link = (struct s_room **)malloc(sizeof(struct s_room *) * 2);
+        lem->room[vertex]->link[1] = 0;
+        lem->room[vertex]->link[0] = add;
+    }
+    return (0);
+}
+
+int validation(t_lem *lem)
+{
+    char *line;
+    int i;
+
+    i = 0;
+    char **res;
+    lem->ant_qnt = 0;
+    lem->room_input = 0;
+    while (get_next_line(0, &line) == 1)
+    {
+        if (line[0] == '#' && ft_strcmp(line, "##start") != 0 && ft_strcmp(line, "##end") != 0)
+        {
+            free(line);
+            continue ;
+        }
+        if (lem->ant_qnt == 0)
+        {
+            if (check_ants(line, lem))
+                return (-1);
+        }
+        else
+        {
+            i = 0;
+            res = ft_strsplit(line, '-');
+            while (res[i] != 0)
+                i++;
+            if (i == 2)
+            {
+                    lem->room_input = 1;
+                    link_realloc(lem, lem->room[0], 1);
+                    link_realloc(lem, lem->room[1], 0);
+                    ft_printf("rooms end\n");
+            }
+            if (lem->room_input == 0)
+            {
+                if (check_rooms(&line, lem))
+                    return (-1);
+            }
+        }
+        free (line);
+    }
 }
 
 int main()
 {
     t_lem lemin;
 
-    lemin.distance = 0;
+   lemin.distance = 0;
 
     lemin.ant_qnt = 3;
-    lemin.room_qnt = 4;
-    lemin.room = (t_room **)malloc(sizeof(t_room *) * (lemin.room_qnt + 1));
+    //lemin.room_qnt = 4;
+    lemin.start = NULL;
+    lemin.end = NULL;
+
+   /* lemin.room = (t_room **)malloc(sizeof(t_room*) * (lemin.room_qnt + 1));
     lemin.room[lemin.room_qnt] = 0;
     lemin.room[0] = (t_room *)malloc(sizeof(t_room));
     lemin.room[1] = (t_room *)malloc(sizeof(t_room));
@@ -87,10 +304,6 @@ int main()
     lemin.room[2]->distance = INF;
     lemin.room[3]->distance = INF;
 
-    lemin.room[0]->room_id = 0;
-    lemin.room[1]->room_id = 1;
-    lemin.room[2]->room_id = 2;
-    lemin.room[3]->room_id = 3;
 
     lemin.room[0]->label = "0";
     lemin.room[1]->label = "1";
@@ -126,8 +339,12 @@ int main()
     while (lemin.room[i] != 0)
         printf("distance : %d\n", lemin.room[i++]->distance);
     i = 0;
-    printf("\n");
-    find_route(lemin.end, &lemin);
+    printf("\n");*/
+    int i = 0;
+    //find_route(lemin.end, &lemin);
+    //while (lemin.room[i] != 0)
+      //  printf("elem %d distance : %d\n", i, lemin.room[i++]->distance);
+    validation(&lemin);
     while (lemin.room[i] != 0)
         printf("elem %d distance : %d\n", i, lemin.room[i++]->distance);
     return (0);
